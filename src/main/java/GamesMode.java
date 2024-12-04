d0336502a3f41a981a75f09dae84fe235deacd5c
Gob2005 <8230153@estg.ipp.pt>
2024-12-04 11:45:28 +0000

    commit

---
+++ src/main/java/GamesMode.java
@@ -0,0 +1,18 @@
+import GameEngine.GameMode;
+
+public class GamesMode implements GameMode {
+    @Override
+    public void automatic() {
+        
+    }
+
+    @Override
+    public void manual() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- src/main/java/Rounds.java
+++ src/main/java/Rounds.java
@@ -1,3 +1,4 @@
+import Exceptions.EmptyCollectionException;
 import GameEngine.Round;
 import Pessoa.Pessoa;
 import Pessoa.Inimigo;
@@ -6,27 +7,57 @@ import Pessoa.ToCruz;
 public class Rounds implements Round {
 
     private Pessoa pessoa;
+    private boolean toCruz;
 
     public Rounds(Pessoa pessoa) {
         this.pessoa = pessoa;
+        if(pessoa instanceof ToCruz){
+            toCruz = true;
+        }else {
+            toCruz = false;
+        }
+
     }
 
     @Override
     public void move() {
-
+        if(toCruz){
+            //só pode fazer dois movimentos, andar para a frente ou andar para tras
+        }else{
+            /*
+            Tem 5 opções de movimento, duas para tras, uma para tras
+            ficar no mesmo sitio, andar uma para a frente, andar duas para a frente
+         */
+        }
     }
 
     @Override
-    public void attack() {
-
+    public void attack(Sala sala) throws EmptyCollectionException {
+        if (sala.getInimigos().lenght() == 0){
+            throw new EmptyCollectionException("A sala não tem inimigos para atacar");
+        }
+        ToCruz toCRUZ = (ToCruz) pessoa;//está errado, pois pessoa pode ser inimigo
+        Inimigo[] inimigos = new Inimigo[sala.getInimigos().lenght()];
+        for (i = 0; i < sala.getInimigos().lenght(); i++) {
+            inimigos[i] = sala.getInimigos()[i];
+        }
+        if(toCruz){
+            for(Inimigo inimigo : inimigos){
+                inimigo.getVida() -= toCRUZ.getPoder();
+            }
+        }else{
+            for(Inimigo inimigo : inimigos){
+                toCRUZ.getVida() -= inimigo.getPoder();
+            }
+        }
     }
 
     @Override
     public void useItem() throws IllegalArgumentException{
-        if(pessoa instanceof Inimigo){
+        if(!toCruz){
             throw new IllegalArgumentException("Inimigo não pode usar item");
         }
-        ToCruz toCruz = (ToCruz) pessoa;
-        toCruz.usarItem();
+        ToCruz toCRUZ = (ToCruz) pessoa;
+        toCRUZ.usarMedkit();
     }
 }
