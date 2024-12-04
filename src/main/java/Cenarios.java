da35ad61e29e5bd597cbb25408c049da147570a6
Btx69-jpg <8230127@estg.ipp.pt>
2024-12-04 20:37:52 +0000

    Update Cenarios.java

--- src/main/java/Cenarios.java
+++ src/main/java/Cenarios.java
@@ -4,29 +4,31 @@ import Item.Item;
 import LinkedList.LinearLinkedUnorderedList;
 import Pessoa.*;
 
+import java.util.Iterator;
+
 public class Cenarios implements Cenario {
 
-    public static void Confronto(ToCruz p1, Inimigo[] p2) {
-        System.out.print("Confronto entre " + p1.getNome() + " e ");
-        for (int i = 0; i < p2.length; i++) {
-            System.out.print( p2[i].getNome());
+    public static boolean Confronto(ToCruz toCruz, LinearLinkedUnorderedList<Inimigo> p2) {
+        Iterator<Inimigo> inimigosIterator = p2.iterator();
+        Inimigo inimigo = inimigosIterator.next();
+        System.out.print("Confronto entre " + toCruz.getNome() + " e " + inimigo);
+        while (inimigosIterator.hasNext()) {
+            inimigo = inimigosIterator.next();
+            System.out.print( ", " + inimigo );
         }
-        System.out.println("Confronto entre " + p1.getNome() + " e " + p2.getNome());
-        System.out.println("Vida inicial de " + p1.getNome() + ": " + p1.getVida());
-        System.out.println("Vida inicial de " + p2.getNome() + ": " + p2.getVida());
-        System.out.println("Poder de " + p1.getNome() + ": " + p1.getPoder());
-        System.out.println("Poder de " + p2.getNome() + ": " + p2.getPoder());
+        System.out.println();
+
         System.out.println("Início do confronto");
-        while (p1.getVida() > 0 && p2.getVida() > 0) {
-            p1.setVida(p1.getVida() - p2.getPoder());
-            p2.setVida(p2.getVida() - p1.getPoder());
-            System.out.println(p1.getNome() + " atacou " + p2.getNome() + " com poder " + p1.getPoder());
-            System.out.println(p2.getNome() + " atacou " + p1.getNome() + " com poder " + p2.getPoder());
-            System.out.println("Vida de " + p1.getNome() + ": " + p1.getVida());
+        while (toCruz.getVida() > 0 && p2. > 0) {
+            toCruz.setVida(toCruz.getVida() - p2.getPoder());
+            p2.setVida(p2.getVida() - toCruz.getPoder());
+            System.out.println(toCruz.getNome() + " atacou " + p2.getNome() + " com poder " + toCruz.getPoder());
+            System.out.println(p2.getNome() + " atacou " + toCruz.getNome() + " com poder " + p2.getPoder());
+            System.out.println("Vida de " + toCruz.getNome() + ": " + toCruz.getVida());
             System.out.println("Vida de " + p2.getNome() + ": " + p2.getVida());
         }
-        if (p1.getVida() <= 0) {
-            System.out.println(p1.getNome() + " foi derrotado");
+        if (toCruz.getVida() <= 0) {
+            System.out.println(toCruz.getNome() + " foi derrotado");
         } else {
             System.out.println(p2.getNome() + " foi derrotado");
         }
