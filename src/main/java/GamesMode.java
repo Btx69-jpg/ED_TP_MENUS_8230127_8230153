1ff743fd38b5ef7153203543c568474e0ae6ebe3
Gob2005 <8230153@estg.ipp.pt>
2024-12-04 11:56:53 +0000

    commit

--- src/main/java/GamesMode.java
+++ src/main/java/GamesMode.java
@@ -3,12 +3,13 @@ import GameEngine.GameMode;
 public class GamesMode implements GameMode {
     @Override
     public void automatic() {
-        
+        //todas as decisões são tomadas automaticamente
+        //iterator the shortestpath
     }
 
     @Override
     public void manual() {
-
+        //aparecer um menu para deixar o utilizador escolher o que fazer
     }
 
     @Override
