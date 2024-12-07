114c0b4cc3b22d7879668043676cf438c714e3f6
Gob2005 <8230153@estg.ipp.pt>
2024-12-07 18:39:24 +0000

    a

--- src/main/java/GamesMode.java
+++ src/main/java/GamesMode.java
@@ -107,9 +107,7 @@ public class GamesMode implements GameMode {
 
     @Override
     public void run() {
-
-        new Missao();
-        Missao missao = Json.ReadJson("/teste.json");
+        this.missao = Json.ReadJson("C:/Users/Gonçalo/Documents/GitHub/ED_TP_8230127_8230153/ED_TP_MENUS_8230127_8230153/src/main/resources/teste.json");
         manual();
     }
 }
