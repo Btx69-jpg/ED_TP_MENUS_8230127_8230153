7641ac1b9aea85ea5893f9e167ca0e38bb1e5669
Gob2005 <8230153@estg.ipp.pt>
2024-12-09 19:37:15 +0000

    a

---
+++ src/main/java/GrafoRenderer.java
@@ -0,0 +1,81 @@
+import Edificio.Sala;
+import Graphs.PropriaAutoria.GraphNetworkEM;
+import LinkedList.LinearLinkedUnorderedList;
+import Missao.Missao;
+
+import java.awt.*;
+import java.util.Iterator;
+import java.util.Random;
+import javax.swing.JPanel;
+
+public class GrafoRenderer extends JPanel {
+
+    private Missao missao;
+    private GraphNetworkEM<Sala> grafo; // Sua classe de grafo
+    private Random random;
+
+    public GrafoRenderer(Missao missao) {
+        this.missao = missao;
+        this.random = new Random();
+        grafo = missao.getEdificio().getSalas();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        int width = getWidth();
+        int height = getHeight();
+
+        LinearLinkedUnorderedList<Sala> vertices = new LinearLinkedUnorderedList<>();
+        Iterator<Sala> itvertices = grafo.getVerticesIterator();
+        while (itvertices.hasNext()) {
+            vertices.addToRear(itvertices.next());
+        }
+
+        Point[] coordenadas = new Point[vertices.size()];
+        Iterator<Sala> itVertices = vertices.iterator();
+        int index = 0;
+
+        // Atribuir coordenadas aleatórias aos vértices
+        while (itVertices.hasNext()) {
+            Sala sala = itVertices.next();
+            int x = random.nextInt(width - 100) + 50;
+            int y = random.nextInt(height - 100) + 50;
+            coordenadas[index++] = new Point(x, y);
+        }
+
+        // Desenhar Arestas
+        Iterator<Sala> itArestas = vertices.iterator();
+        while (itArestas.hasNext()) {
+            Sala origem = itArestas.next();
+            int origemIndex = grafo.getIndex(origem);
+
+            // Obter vértices conectados a partir da origem
+            LinearLinkedUnorderedList<Sala> conexoes = grafo.getConnectedVertices(origem);
+            Iterator<Sala> itConexoes = conexoes.iterator();
+
+            while (itConexoes.hasNext()) {
+                Sala destino = itConexoes.next();
+                int destinoIndex = grafo.getIndex(destino);
+
+                Point p1 = coordenadas[origemIndex];
+                Point p2 = coordenadas[destinoIndex];
+
+                // Desenhar linha entre os vértices
+                g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
+            }
+
+            // Desenhar Vértices
+            itVertices = vertices.iterator();
+            index = 0;
+            while (itVertices.hasNext()) {
+                Sala sala = itVertices.next();
+                Point p = coordenadas[index++];
+                g2d.fillOval(p.x - 10, p.y - 10, 20, 20); // Círculo do vértice
+                g2d.drawString(sala.getNome(), p.x - 15, p.y - 15); // Nome do vértice
+            }
+        }
+    }
+}
