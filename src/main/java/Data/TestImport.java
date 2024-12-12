8db419df63de044b595135be6315c12b4d213495
Btx69-jpg <8230127@estg.ipp.pt>
2024-12-12 12:21:46 +0000

    a

--- src/main/java/Data/TestImport.java
+++ src/main/java/Data/TestImport.java
@@ -4,7 +4,7 @@ import Missao.Missao;
 public class TestImport {
     public static void main(String[] args) {
         Missao missao = new Missao();
-         DataTreating.ReadMissao("C:\\Faculdade\\2ano\\PrimeiroSemestre\\ED\\dadosJogo.json");
+         DataTreating.ReadMissao("C:\\Faculdade\\2ano\\PrimeiroSemestre\\ED\\Trabalho pratico\\ED_TP_MENUS\\ED_TP_MENUS_8230127_8230153\\src\\main\\resources\\teste.json");
         missao = DataTreating.getMissaoByVersion(1);
         System.out.println(missao);
     }
