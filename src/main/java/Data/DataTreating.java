ad11e003205743781cd7d50fa551370429848ad6
Btx69-jpg <8230127@estg.ipp.pt>
2024-12-13 10:31:46 +0000

    Update DataTreating.java

--- src/main/java/Data/DataTreating.java
+++ src/main/java/Data/DataTreating.java
@@ -3,6 +3,7 @@ package Data;
 import Edificio.Edificio;
 import Edificio.Sala;
 import Enum.ItemType;
+import Exceptions.ElementNotFoundException;
 import Exceptions.EmptyCollectionException;
 import Graphs.GraphNetwork;
 import Item.Item;
@@ -29,7 +30,10 @@ public class DataTreating {
     private static LinearLinkedOrderedList<Missao> missoes = new LinearLinkedOrderedList<>();
     public static Relatorios relatorios = new Relatorios();
 
-    public static Missao getMissaoByVersion(int Version) {
+    public static Missao getMissaoByVersion(int Version) throws IllegalArgumentException  {
+        if (Version <= 0){
+            throw new IllegalArgumentException("A versão não pode ser negativa/nula");
+        }
         for (Missao missao : missoes) {
             if (missao.getVersion() == Version) {
                 return missao.clone();
@@ -37,31 +41,57 @@ public class DataTreating {
         }
         return null;
     }
-    public static void removeMissaoByVersion(int Version) {
+
+    public static void removeMissaoByVersion(int Version) throws IllegalArgumentException, EmptyCollectionException, ElementNotFoundException {
+        if (Version <= 0){
+            throw new IllegalArgumentException("A versão não pode ser negativa/nula");
+        }
         for (Missao missao : missoes) {
             if (missao.getVersion() == Version) {
-                missoes.remove(missao);
+                try {
+                    missoes.remove(missao);
+                } catch (EmptyCollectionException e) {
+                    throw new EmptyCollectionException("O Jogo ainda não possui missões");
+                } catch (ElementNotFoundException e) {
+                    throw new ElementNotFoundException("O jogo não possui nenhuma missão com esta Versão");
+                }
+
             }
         }
     }
 
-    public static void removeMissao(Missao missaoremove) {
+    public static void removeMissao(Missao missaoremove) throws IllegalArgumentException, ElementNotFoundException, EmptyCollectionException{
+        if (missaoremove == null){
+            throw new IllegalArgumentException("A missão não pode ser nula");
+        }
+
         for (Missao missao : missoes) {
             if (missao.equals(missaoremove)) {
-                missoes.remove(missaoremove);
+                try {
+                    missoes.remove(missaoremove);
+                } catch (EmptyCollectionException e) {
+                    throw new EmptyCollectionException("O Jogo ainda não possui missões");
+                } catch (ElementNotFoundException e) {
+                    throw new ElementNotFoundException("O jogo não possui esta missão");
+                }
+
             }
         }
     }
 
-    public static LinearLinkedOrderedList<Missao> getMissoes() {
+    public static LinearLinkedOrderedList<Missao> getMissoes() throws NullPointerException {
+        if (missoes == null || missoes.isEmpty()){
+            throw new NullPointerException("O jogo ainda não possui missões");
+        }
+
         LinearLinkedOrderedList<Missao> missoesclone = new LinearLinkedOrderedList<>();
         for (Missao missao : missoes) {
-                missoesclone.add(missao.clone());
+            missoesclone.add(missao.clone());
         }
         return missoesclone;
     }
 
-    public static void addRelatorio(Relatorio relatorio){
+    public static void addRelatorio(Relatorio relatorio) throws NullPointerException{
         relatorios.addRelatorio(relatorio);
     }
 
